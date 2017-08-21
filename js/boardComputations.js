5cf5db0d420f7c9b95a92eef421b519309686170
Dee G <Dereje1>
2017-08-21 14:22:28 -0400

    separate computations script,control panel, elementsize var

---
+++ js/boardComputations.js
@@ -0,0 +1,70 @@
+function conway(alive,boxSize){
+  let newState=[];
+  for(let i=0;i<boxSize;i++){
+    for(let j=0;j<boxSize;j++){
+      let cellId = i.toString()+"_"+j.toString();
+      let totalLiveNb = liveNeighbours(cellId,alive,boxSize)
+
+      if(alive.includes(cellId)){
+        if (totalLiveNb === 2){newState.push(cellId)}
+        if (totalLiveNb === 3){newState.push(cellId)}
+      }
+      else{
+        if(totalLiveNb===3){
+          newState.push(cellId)
+        }
+      }
+    }
+  }
+
+  return newState;
+}
+
+function liveNeighbours(cell,alive,boxSize){
+  let liveNeighbourCount=0;
+  let allNeigbhours = possibleNeighbours(cell,boxSize)
+  for (let i=0;i<alive.length;i++){
+    if(!allNeigbhours.includes(alive[i])){continue;}
+    else{liveNeighbourCount++;}
+  }
+  return liveNeighbourCount
+}
+
+function possibleNeighbours(cell,boxSize){
+  let cellCoordinates = cell.split("_");
+  cellCoordinates=cellCoordinates.map(function(u){
+    return parseInt(u,10)
+  })
+  let possibleNeighbours=[
+    //top 3
+    [cellCoordinates[0]-1,cellCoordinates[1]-1],
+    [cellCoordinates[0]-1,cellCoordinates[1]],
+    [cellCoordinates[0]-1,cellCoordinates[1]+1],
+    //middle 2
+    [cellCoordinates[0],cellCoordinates[1]-1],
+    [cellCoordinates[0],cellCoordinates[1]+1],
+    //bottom 3
+    [cellCoordinates[0]+1,cellCoordinates[1]-1],
+    [cellCoordinates[0]+1,cellCoordinates[1]],
+    [cellCoordinates[0]+1,cellCoordinates[1]+1],
+  ]
+  possibleNeighbours=possibleNeighbours.map(function(u){
+    if (u[0]<0){u[0]=boxSize-1}
+    if (u[1]<0){u[1]=boxSize-1}
+    if (u[0]>(boxSize-1)){u[0]=0}
+    if (u[1]>(boxSize-1)){u[1]=0}
+    return (u[0].toString()+"_"+u[1].toString())
+  })
+  return possibleNeighbours;
+}
+
+function simulator(param){
+  let newState=[]
+  let areaCovered = Math.floor(.9*param*param)
+  for (let i=0;i<areaCovered;i++){
+    let y = Math.floor(Math.random() * param)
+    let x = Math.floor(Math.random() * param)
+    newState.push(y.toString()+"_"+x.toString())
+  }
+ return newState;
+}
--- js/index.js
+++ js/index.js
@@ -2,72 +2,131 @@ class Main extends React.Component{
   constructor(props){
     super(props)
     this.state={
-      board:[],
+      alive:[],
       elapsedTime:0,
-      updateInterval:20
+      generations:0,
+      updateInterval:50,
+      elementSize:20
     }
-    this.boardstate=this.boardstate.bind(this)
+    this.manualStateChange=this.manualStateChange.bind(this)
   }
   componentDidMount(){
     console.log("Mounted")
-    this.intervalId = setInterval(this.testtimer.bind(this), this.state.updateInterval);
+    this.setState({
+        alive:simulator(this.state.elementSize)
+      })
+    this.intervalId = setInterval(this.tick.bind(this), this.state.updateInterval);
   }
   componentWillUnmount(){
     clearInterval(this.intervalId);
   }
-  testtimer(){
-    this.boardstate(0,simulator())
-    //console.log(this.state.elapsedTime)
-    this.setState({elapsedTime:(this.state.elapsedTime+=this.state.updateInterval)})
-    if(this.state.elapsedTime>20000){clearInterval(this.intervalId)}
+  tick(){
+    this.setState({
+        alive:conway(this.state.alive,this.state.elementSize),
+        elapsedTime:this.state.elapsedTime+=this.state.updateInterval,
+        generations:this.state.generations+1
+      })
+    //if(this.state.generations>=100){clearInterval(this.intervalId)}
   }
-  boardstate(e,cellData){
+  manualStateChange(e,cellData){
     //console.log(cellData)
-    let x =this.state.board.slice();
+    let x =this.state.alive.slice();
     if (x.includes(cellData)){
       x.splice(x.indexOf(cellData),1)
     }
     else{
       x.push(cellData)
     }
-    this.setState({ board:x})
+    this.setState({ alive:x})
 
   }
 
   elementbuilder(){
     let x=[],bcol;
-
-    for (let i=0;i<20;i++){
-      for(let j=0;j<20;j++){
+    let elementWidth = (500/this.state.elementSize).toString()+"px"
+    for (let i=0;i<this.state.elementSize;i++){
+      for(let j=0;j<this.state.elementSize;j++){
           let idConstruct = i.toString()+"_"+j.toString();
-          if(this.state.board.includes(idConstruct)){bcol="blue"}
+          if(this.state.alive.includes(idConstruct)){bcol="#2f47fc"}
           else{bcol="white"}
-          x.push(   <div
+          x.push(   <span
                      id={idConstruct}
-                     onClick={(e)=>this.boardstate(e,idConstruct)}
+                     onClick={(e)=>this.manualStateChange(e,idConstruct)}
                      className="Elemental"
-                     style={{background: bcol}}
+                     style={{background: bcol,
+                            width:elementWidth,
+                            height:elementWidth,
+                     }}
                      />
           )
       }
     }
     return x;
   }
-
+  //Control Panel Actions below
+  reset(){
+    clearInterval(this.intervalId);
+    this.setState({
+        alive:simulator(this.state.elementSize),
+        elapsedTime:0,
+        generations:0
+      })
+    this.intervalId = setInterval(this.tick.bind(this), this.state.updateInterval);
+  }
+  pause(){
+    clearInterval(this.intervalId);
+  }
+  play(){
+    this.intervalId = setInterval(this.tick.bind(this), this.state.updateInterval);
+  }
+  dump(){
+    clearInterval(this.intervalId);
+    this.setState({
+        alive:[],
+        elapsedTime:0,
+        generations:0
+      })
+  }
   render(){
     return(
-      <div id="elementHolder">
-        {this.elementbuilder()}
-
+      <div>
+        <ControlPanel
+          generations={this.state.generations}
+          onReset = {()=>this.reset()}
+          onPause = {()=>this.pause()}
+          onPlay = {()=>this.play()}
+          onDump = {()=>this.dump()}
+        />
+        <div id="elementHolder">{this.elementbuilder()}</div>
       </div>
     )
   }
 }
 
-function simulator(){
-  let y = Math.floor(Math.random() * 19) + 1
-  let x = Math.floor(Math.random() * 19) + 1
-  return(y.toString()+"_"+x.toString())
+class ControlPanel extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  playPause(){
+    let ButtonGroup = ReactBootstrap.ButtonGroup ;
+    let Button = ReactBootstrap.Button ;
+    return(
+      <ButtonGroup>
+        <Button onClick={this.props.onPlay}><i className="fa fa-play" aria-hidden="true"></i></Button>
+        <Button onClick={this.props.onPause}><i className="fa fa-pause" aria-hidden="true"></i></Button>
+        <Button onClick={this.props.onReset}><i className="fa fa-refresh" aria-hidden="true"></i></Button>
+        <Button onClick={this.props.onDump}><i className="fa fa-trash" aria-hidden="true"></i></Button>
+      </ButtonGroup>
+    )
+  }
+  render(){
+    return(
+      <div id="BoardInfo">
+        <h3>{this.props.generations}</h3>
+        {this.playPause()}
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
